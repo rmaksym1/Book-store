000f6f55968e9f2f73f6b94fa240c39057472753
Maks <maksimronkanen940@gmail.com>
2025-12-16 19:11:46 +0200

    Added BookDto, CreateBookRequestDto, BookMapper, EntityNotFoundException, BookController, MapperConfig. Fixed IntroApplication, added necessary methods in BookRepository, BookService classes

--- src/main/java/com/origin/bookstore/model/Book.java
+++ src/main/java/com/origin/bookstore/model/Book.java
@@ -5,11 +5,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import java.math.BigDecimal;
 import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import lombok.Getter;
 import lombok.Setter;
 
 @Entity
+@Getter
 @Setter
 @Table(name = "books")
 public class Book {
