1ffab9c9ff83bb295879b6cd4d533430d47d1e08
Maks <maksimronkanen940@gmail.com>
2025-12-31 21:21:06 +0200

    Added CustomGlobalExceptionHandler, and Data Validation in DTO classes

--- src/main/java/com/origin/bookstore/dto/CreateBookRequestDto.java
+++ src/main/java/com/origin/bookstore/dto/CreateBookRequestDto.java
@@ -1,5 +1,10 @@
 package com.origin.bookstore.dto;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,9 +12,17 @@ import lombok.Setter;
 @Getter
 @Setter
 public class CreateBookRequestDto {
+    @NotBlank
+    @Size(min = 1, max = 100)
     private String title;
+    @NotBlank
+    @Size(min = 1, max = 100)
     private String author;
+    @NotBlank
+    @Pattern(regexp = "\\d{3}-\\d{10}")
     private String isbn;
+    @NotNull
+    @DecimalMin(value = "0", inclusive = true)
     private BigDecimal price;
     private String description;
     private String coverImage;
