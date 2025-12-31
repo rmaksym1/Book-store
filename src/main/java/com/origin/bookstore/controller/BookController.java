757f9dfaaec4527b9eaea23cc7014ebab4649d49
Maks <maksimronkanen940@gmail.com>
2025-12-31 20:35:50 +0200

    Added CustomGlobalExceptionHandler, and Data Validation in DTO classes

--- src/main/java/com/origin/bookstore/controller/BookController.java
+++ src/main/java/com/origin/bookstore/controller/BookController.java
@@ -4,6 +4,7 @@ import com.origin.bookstore.dto.BookDto;
 import com.origin.bookstore.dto.BookSearchParameters;
 import com.origin.bookstore.dto.CreateBookRequestDto;
 import com.origin.bookstore.service.BookService;
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -34,7 +35,7 @@ public class BookController {
     }
 
     @PostMapping
-    public BookDto createBook(@RequestBody CreateBookRequestDto bookDto) {
+    public BookDto createBook(@RequestBody @Valid CreateBookRequestDto bookDto) {
         return bookService.save(bookDto);
     }
 
@@ -45,7 +46,7 @@ public class BookController {
     }
 
     @PutMapping("/{id}")
-    public BookDto updateBook(@PathVariable Long id, @RequestBody BookDto bookDto) {
+    public BookDto updateBook(@PathVariable Long id, @RequestBody @Valid BookDto bookDto) {
         return bookService.updateBook(id, bookDto);
     }
 
