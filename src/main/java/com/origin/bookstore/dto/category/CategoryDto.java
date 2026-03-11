ed1d1bbfdf40bbc7d6a1ecc4c87d040a1893fd95
Maks <maksimronkanen940@gmail.com>
2026-03-11 17:01:11 +0300

    converted DTOs to java records to clean up boilerplate code

--- src/main/java/com/origin/bookstore/dto/category/CategoryDto.java
+++ src/main/java/com/origin/bookstore/dto/category/CategoryDto.java
@@ -1,12 +1,7 @@
 package com.origin.bookstore.dto.category;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-public class CategoryDto {
-    private Long id;
-    private String name;
-    private String description;
-}
+public record CategoryDto(
+        Long id,
+        String name,
+        String description
+) {}
--- src/main/java/com/origin/bookstore/dto/category/CreateCategoryRequestDto.java
+++ src/main/java/com/origin/bookstore/dto/category/CreateCategoryRequestDto.java
@@ -2,14 +2,10 @@ package com.origin.bookstore.dto.category;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
-public class CreateCategoryRequestDto {
-    @NotBlank
-    @Size(min = 1, max = 100)
-    private String name;
-    private String description;
-}
+public record CreateCategoryRequestDto(
+        @NotBlank
+        @Size(min = 1, max = 100)
+        String name,
+        String description
+) {}
